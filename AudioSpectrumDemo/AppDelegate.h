97f469adc64e69f2421895cfc1915b58a6fe0228
César <424321999@qq.com>
2019-05-08 09:49:43 +0800

    Initial Commit

---
+++ AudioSpectrumDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AudioSpectrumDemo
+//
+//  Created by user on 2019/5/8.
+//  Copyright © 2019 adu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AudioSpectrumDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  AudioSpectrumDemo
+//
+//  Created by user on 2019/5/8.
+//  Copyright © 2019 adu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
